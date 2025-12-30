12b163661ea8019e4a02fc4b8891c54e0f993ea0
凌览 <58327088+CatsAndMice@users.noreply.github.com>
2025-12-30 22:11:40 +0800

    Update home button URL and clear tips text

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // 首屏信息栏按钮文字
   MAGZINE_HOME_BANNER_ENABLE: true, // 首屏右上角的宣传位
   MAGZINE_HOME_BUTTON: true,
-  MAGZINE_HOME_BUTTON_URL: '/about',
+  MAGZINE_HOME_BUTTON_URL: 'http://nologo.code24.top/',
   MAGZINE_HOME_BUTTON_TEXT: '了解更多',
 
   MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
@@ -10,7 +10,7 @@ const CONFIG = {
   MAGZINE_HOME_TITLE: '去水印下载鸭',
   MAGZINE_HOME_DESCRIPTION:
     '免费，无需登录支持快手、小红书、抖音等多个平台快速视频、图片无水印下载',
-  MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
+  MAGZINE_HOME_TIPS: '',
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   MAGZINE_RECOMMEND_POST_TAG: '推荐',
